216f3e3e1465741ea3a8e73bca7cde7649ef0628
dilansakila <dilansakila99@gmail.com>
2024-07-08 21:43:22 +0530

    edited app.jsx

--- bill_invoice/src/App.jsx
+++ bill_invoice/src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import react, {useEffect,useState} from "react";
 import './App.css';
 
 import React from 'react'
@@ -6,7 +6,16 @@ import React from 'react'
 function App() {
   return (
     <>
-    
+    <div className='container'>
+      <div className="form">
+        <div className="input">
+          <input type='text' placeholder='Invoice number' value={} onChange={}/>
+        </div>
+        <div className="button">
+          <button onClick={}>Create</button>
+        </div>
+      </div>
+    </div>
       
     </>
   )
