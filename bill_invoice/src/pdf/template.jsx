a93db714157f7bc78e47cfe093e40366722ef997
dilansakila <dilansakila99@gmail.com>
2024-07-15 17:31:01 +0530

    setup template.jsx

--- bill_invoice/src/pdf/template.jsx
+++ bill_invoice/src/pdf/template.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import ReactToPrint from "react-to-print";
+import React, {useRef,useState,useEffect} from 'react';
+import {Close} from '@mui/icons-material'
+import {Dialog,DialogTitle,DialogContent} from '@mui/material'
+
 
 const template = () => {
   return (
