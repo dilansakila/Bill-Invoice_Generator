4f8c57eba9c76dd9c485cbb1050375b8564ae4f1
dilansakila <dilansakila99@gmail.com>
2024-07-16 22:31:19 +0530

    updated Template.jsx

--- bill_invoice/src/pdf/template.jsx
+++ bill_invoice/src/pdf/template.jsx
@@ -2,14 +2,47 @@ import ReactToPrint from "react-to-print";
 import React, {useRef,useState,useEffect} from 'react';
 import {Close} from '@mui/icons-material'
 import {Dialog,DialogTitle,DialogContent} from '@mui/material'
+import barcode from "react-barcode";
 
+function pdfTemplate (props) {
+ const ref =useRef();
+ const [openAirPopup] = usestate(false);
 
-const template = () => {
-  return (
-    <div>
-      
+ const [Item, setItem ] = useState('');
+ const [Amount,setAmount] = useState(0);
+
+ const [List,setList] = useState([]);
+
+ const addData = ()=>{
+  list.push({
+    product:Item,
+    amount:Amount,
+  })
+ }
+ let sum=0;
+ list.forEach(amount=> {
+  sum += parseInt(amount.amount)
+ })
+
+ return(
+  <>
+  <div className="container" ref={ref}>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-md-4 barcode">
+              <Barcode value={'4n%${props.InvoiceNumber}+ut%'} width={1} height={50} displayValue={false} />
+               
+            </div>
+          </div>
+        </div>
+    
+      </div>
     </div>
-  )
+  </div>
+  </>
+ )
 }
 
-export default template
+
