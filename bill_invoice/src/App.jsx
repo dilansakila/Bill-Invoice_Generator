f54fb7b73b717ff2f2fa8a51de1eb655c7608ebd
dilansakila <dilansakila99@gmail.com>
2024-07-18 09:43:00 +0530

    updated App.jsx

--- bill_invoice/src/App.jsx
+++ bill_invoice/src/App.jsx
@@ -9,20 +9,33 @@ function App() {
   const [Dates,setDates]=useState('');
   const [view,setView] = useState(true);
 
+  useEffect (()=>{
+    const current=new Date();
+    const data = '${current.getData()}/${current.getMonth()+1}/${current.getFullYear()}'
+    setDates(date);
+  },[])
+
+  const create =()=>{
+    setView(true);
+  }
+
 
   return (
     <>
+    {
+      view?
     <div className='container'>
       <div className="form">
         <div className="input">
-          <input type='text' placeholder='Invoice number' value={} onChange={}/>
+          <input type='text' placeholder='Invoice number' value={InvoiceNumber} onChange={(e)=>setInvoiceNumber(e.target.value)}/>
         </div>
         <div className="button">
           <button onClick={}>Create</button>
         </div>
       </div>
     </div>
-      
+    :<pdfTemplate InvoiceNumber={InvoiceNumber} date={Dates}/>
+}
     </>
   )
 }
